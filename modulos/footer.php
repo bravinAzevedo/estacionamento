9f0eb658f410a85e4f8b9c6c4c5235a32c8c3fdf
Marco Aikau <48110286+marcoaikau@users.noreply.github.com>
2023-07-06 09:45:27 -0300

    Add files via upload

---
+++ modulos/footer.php
@@ -0,0 +1,18 @@
+<footer>
+        <img src="img/logo.png">
+        <ul class="lista">
+            <li class="itens"><a href="estacionamento.php">Home</a></li>
+            <li class="itens"><a href="parking.php">Parking</a></li>
+            <li class="itens"><a href="pagamentos.php">Parking.com</a></li>
+            <li class="itens"><a href="atendimento.php">Atendimento</a></li>
+        </ul>
+        <div class="redessociais">
+            <ul class="lista2">
+                <li class="icones"><a href="https://www.facebook.com/profile.php?id=100091836685763"><img src="./img/facebook.png"/></a></li>
+                <li class="icones"><a href="https://www.instagram.com/parking_klm"><img src="img/instagram.png"/></a></li>
+                <li class="icones"><a href="https://www.linkedin.com/in/parking-estacionamentos-15223a274"><img src="./img/linkedin.png"/></a></li>
+            </ul>
+        </div>
+        <h4>Direitos Autorais 2023 - Parking.com</h4>
+        <h4>CNPJ: 80.520.963/0001-00</h4>
+    </footer>
---
+++ modulos/topo.php
@@ -0,0 +1,22 @@
+<body>
+    <input type="checkbox" id="chec">
+    <label for="chec">
+        <img src="img/menu2.png">
+
+    </label>
+    <nav>
+        <ul class="menu">
+            <li><a href="estacionamento.php">Home</a></li>
+            <li><a href="cadastro.php">Reserva</a></li>
+            <li><a href="cadastro.php">Parking.com</a></li>
+            <li><a href="pagamentos.php">Pagamentos</a></li>
+            <li><a href="atendimento.php">Atendimento</a></li>
+        </ul>
+    </nav>
+    <header class="menu-principal">
+        <div class="header-1">
+            <div class="logo">
+                <img src="img/logo.png"/>
+            </div>
+        </div>
+    </header>
